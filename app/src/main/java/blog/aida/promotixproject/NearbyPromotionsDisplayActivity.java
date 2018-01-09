d1ee8d9b7b0f76201a742dac1310ea67c9b10a67
aidadenisa <aida.denisa@yahoo.com>
2018-01-09 03:14:06 +0200

    [toolbar] Added a drawable menu with a header and some items

--- app/src/main/java/blog/aida/promotixproject/NearbyPromotionsDisplayActivity.java
+++ app/src/main/java/blog/aida/promotixproject/NearbyPromotionsDisplayActivity.java
@@ -18,9 +18,12 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ListView;
@@ -94,7 +97,8 @@ public class NearbyPromotionsDisplayActivity extends AppCompatActivity implement
 
     private ArrayList<Store> stores = new ArrayList<Store>();
 
-
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -136,6 +140,23 @@ public class NearbyPromotionsDisplayActivity extends AppCompatActivity implement
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open, R.string.closed);
+
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void setUpMapIfNeeded() {
@@ -260,7 +281,6 @@ public class NearbyPromotionsDisplayActivity extends AppCompatActivity implement
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mGoogleApiClient.connect();
-
     }
 
 
